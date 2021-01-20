7b619dce22996a319b97e944a4e8def56741f81c
vladsaveliev <vladislav.sav@gmail.com>
2021-01-20 13:11:16 +1100

    Parametrize hail_subset_gnomad.py

--- hail_subset_gnomad.py
+++ hail_subset_gnomad.py
@@ -1,21 +1,24 @@
-import random
+import os
 import hail as hl
 import pandas as pd
+import click
 
-hl.init(default_reference="GRCh38")
+mt_src_path = \
+    'gs://gcp-public-data--gnomad/release/3.1/mt/genomes/' \
+    'gnomad.genomes.v3.1.hgdp_1kg_subset_dense.mt/'
 
-N = 50
-src_path = "gs://gcp-public-data--gnomad/release/3.1/mt/genomes/gnomad.genomes.v3.1.hgdp_1kg_subset_dense.mt/"
-trg_path = "gs://playground-us-central1/cpg-fewgenomes/subset_from_gnomad.mt"
-# gsutil cp datasets/50genomes/samples.ped gs://playground-us-central1/cpg-fewgenomes/samples.ped
-samples_csv_fpath = "gs://playground-us-central1/cpg-fewgenomes/samples.ped"
-df = pd.read_csv(samples_csv_fpath, sep='\t')
-samples = df['Individual.ID']
+@click.command()
+@click.argument('ped_path')
+def main(ped_path):
+    df = pd.read_csv(ped_path, sep='\t')
+    sample_names = list(df['Individual.ID'])
 
-mt = hl.read_matrix_table(src_path)
-mt = mt.filter_cols(hl.literal(list(samples)).contains(mt['s']))
-mt = mt.drop(*[k for k in mt.globals.dtype.keys()])
-mt = mt.drop(*[k for k in mt.col.dtype.keys() if k != 's'])
-mt = mt.drop(*[k for k in mt.row.dtype.keys() if k not in ['s', 'locus', 'alleles']])
+    mt_subset_path = os.path.join(os.path.dirname(ped_path), 'gnomad.subset.mt')
+    hl.init(default_reference='GRCh38')
+    mt = hl.read_matrix_table(mt_src_path)
+    mt = mt.filter_cols(hl.literal(sample_names).contains(mt['s']))
+    mt = mt.drop(*[k for k in mt.globals.dtype.keys()])
+    mt = mt.drop(*[k for k in mt.col.dtype.keys() if k != 's'])
+    mt = mt.drop(*[k for k in mt.row.dtype.keys() if k not in ['s', 'locus', 'alleles']])
 
-mt.write(trg_path, overwrite=True)
+    mt.write(mt_subset_path, overwrite=True)
