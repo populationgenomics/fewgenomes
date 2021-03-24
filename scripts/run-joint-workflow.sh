9172b041c1f305a2e0dc6a02cceeace61a106364
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-03-25 02:14:41 +1100

    Add run-joint-workflow.sh

---
+++ scripts/run-joint-workflow.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+joint-workflow-batch.py --sample-map gs://cpg-fewgenomes-temporary/joint-calling/100genomes-gvcf-gvcf-local.tsv --output_bucket gs://cpg-fewgenomes-temporary/joint-calling/testrun --callset_name fewgenomes
