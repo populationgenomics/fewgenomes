48ea451be761be4301059615f8a126a30b0efce7
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-03-25 00:44:05 +1100

    Fix

--- scripts/combiner-batch.py
+++ scripts/combiner-batch.py
@@ -19,7 +19,7 @@ batch = hb.Batch(name='dataproc example', backend=service_backend)
 
 dataproc.hail_dataproc_job(
     batch,
-    f'run-python-script.py combiner-on-dataproc.py \
+    f'run-python-script.py combine_gvcfs.py \
 --sample-map gs://cpg-fewgenomes-temporary/joint-calling/50genomes-gcs-au-round1.csv \
 --out-mt gs://cpg-fewgenomes-temporary/test-dataproc-package/50genomes.mt \
 --bucket gs://cpg-fewgenomes-temporary/work/vcf-combiner/test-dataproc-package/ \
