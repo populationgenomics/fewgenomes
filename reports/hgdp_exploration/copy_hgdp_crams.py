35a98ab00d35719fd5b9d859e6eebfa9cd4b5999
pdiakumis <peterdiakumis@gmail.com>
2021-03-30 12:43:50 +1100

    test CRAM copying

--- reports/hgdp_exploration/copy_hgdp_crams.py
+++ reports/hgdp_exploration/copy_hgdp_crams.py
@@ -3,24 +3,31 @@ Copies HGDP CRAM files from 'gs://ibd-external-datasets'
 to the fewgenomes main bucket.
 """
 
-# import os
+import os
 import csv
 import hailtop.batch as hb
 
-# output = os.getenv('OUTPUT')
+# OUTPUT gets propagated from the analysis-runner cli to the server
+output_bucket = os.getenv('OUTPUT')
+assert output_bucket and output_bucket.startswith('gs://cpg-fewgenomes-test/')
+
 input_filelist = './data/filtered65.csv'
 
 def copy_to_bucket(b, sample_name, ftype, fname):
     j = b.new_job(name=f'{sample_name}-{ftype}')
-    j.command(f'echo "copy {ftype} file {fname} for {sample_name}!" > {j.ofile}')
-    b.write_output(j.ofile, f'gs://cpg-peter-dev/batch_runs/test/{sample_name}_{ftype}.txt')
+    # j.command(f'echo "copy {ftype} file {fname} for {sample_name}!" > {j.ofile}')
+    # b.write_output(j.ofile, f'gs://cpg-peter-dev/batch_runs/test/{sample_name}_{ftype}.txt')
+    j.command(f'gsutil cp {fname} {output_bucket}')
 
 backend = hb.ServiceBackend('peterdiakumis-trial', 'cpg-peter-dev')
 b = hb.Batch(backend=backend, name='test-scatter')
 
 with open(input_filelist, newline='') as csvfile:
     reader = csv.DictReader(csvfile)
+    line_count = 0
     for row in reader:
-        copy_to_bucket(b, row["sample_name"], row["ftype"], row["fname"])
+        if line_count < 6:
+            copy_to_bucket(b, row['sample_name'], row['ftype'], row['fname'])
+            line_count += 1
 
 b.run()
