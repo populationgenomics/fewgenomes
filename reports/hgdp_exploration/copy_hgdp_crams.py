830898ae63a250a28e8c06a9caf343851254481a
pdiakumis <peterdiakumis@gmail.com>
2021-04-01 13:17:23 +1100

    which gsutil

--- reports/hgdp_exploration/copy_hgdp_crams.py
+++ reports/hgdp_exploration/copy_hgdp_crams.py
@@ -14,19 +14,6 @@ assert output_bucket and output_bucket.startswith('gs://cpg-fewgenomes-temporary
 
 input_filelist = './data/filtered65.csv'
 
-print('which gcloud?')
-subprocess.run(
-    ['which', 'gcloud']
-)
-print('which gsutil?')
-subprocess.run(
-    ['which', 'gsutil']
-)
-print('gcloud config list:')
-subprocess.run(
-    ['gcloud', 'config', 'list']
-)
-
 def copy_to_bucket(bucket: str, batch: hb.batch.Batch, sample_name: str, ftype: str, fname: str) -> None:
     """
     :param bucket: GCS bucket to copy to
@@ -38,7 +25,11 @@ def copy_to_bucket(bucket: str, batch: hb.batch.Batch, sample_name: str, ftype: 
     j = batch.new_job(name=f'{sample_name}-{ftype}')
     # j.command(f'echo "copy {ftype} file {fname} for {sample_name}!" > {j.ofile}')
     # batch.write_output(j.ofile, f'gs://cpg-peter-dev/batch_runs/test/{sample_name}_{ftype}.txt')
-    j.command(f'gsutil cp {fname} {bucket}')
+    print('which gcloud?')
+    j.command(f'which gcloud')
+    print('which gsutil?')
+    j.command(f'which gsutil')
+    #j.command(f'gsutil cp {fname} {bucket}')
 
 service_backend = hb.ServiceBackend(
     billing_project=os.getenv('HAIL_BILLING_PROJECT'), bucket=os.getenv('HAIL_BUCKET')
