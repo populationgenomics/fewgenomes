d5697da2c6f9bb687c7f212d62e6ca47dc838fb3
pdiakumis <peterdiakumis@gmail.com>
2021-04-01 14:47:37 +1100

    test copy

--- reports/hgdp_exploration/copy_hgdp_crams.py
+++ reports/hgdp_exploration/copy_hgdp_crams.py
@@ -13,6 +13,7 @@ output_bucket = os.getenv('OUTPUT')
 assert output_bucket and output_bucket.startswith('gs://cpg-fewgenomes-temporary/')
 
 input_filelist = './data/filtered65.csv'
+analysis_runner_image = 'australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:45c3f8125e300cd70bb790e32d96816f003a7af2-hail-0.2.64.devcb1c44c7b529'
 
 def copy_to_bucket(bucket: str, batch: hb.batch.Batch, sample_name: str, ftype: str, fname: str) -> None:
     """
@@ -23,22 +24,17 @@ def copy_to_bucket(bucket: str, batch: hb.batch.Batch, sample_name: str, ftype: 
     :param fname: file name to copy (full GCS path)
     """
     j = batch.new_job(name=f'copy-{sample_name}-{ftype}')
-
-    (j.image('australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:45c3f8125e300cd70bb790e32d96816f003a7af2-hail-0.2.64.devcb1c44c7b529')
-      .command(f'gcloud config list > {j.ofile}'))
-    batch.write_output(j.ofile, f'{bucket}/test/{sample_name}_{ftype}_gcloud_config.txt')
-    #j.command(f'which gcloud > {j.ofile}')
-    #batch.write_output(j.ofile, f'{bucket}/{sample_name}_{ftype}123.txt')
-    #print('which gsutil?')
-    #j.command(f'which gsutil')
-    #j.command(f'gsutil cp {fname} {bucket}')
+    j.command(f'gsutil cp {fname} {bucket}')
 
 service_backend = hb.ServiceBackend(
     billing_project=os.getenv('HAIL_BILLING_PROJECT'), bucket=os.getenv('HAIL_BUCKET')
 )
-#backend = hb.ServiceBackend('peterdiakumis-trial', 'cpg-peter-dev')
 b = hb.Batch(backend=service_backend, name='test-cram-copying')
+j_auth = b.new_job(name='authorise service account access')
+(j_auth.image(analysis_runner_image)
+       .command(f'gcloud -q auth activate-service-account --key-file=/gsa-key/key.json'))
 
+# Copy CRAMs and indexes listed in CSV to bucket
 with open(input_filelist, newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     line_count = 0
