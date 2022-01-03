07dbd9a1f773d68f328be7f7dee8fa83c294a7ce
MattWellie <mattwellie@gmail.com>
2022-01-04 10:06:29 +1100

    adding a couple of scripts, and a change in requirements,hopefully temporary

--- datasets/acute-care/extract_trio_vcf.py
+++ datasets/acute-care/extract_trio_vcf.py
@@ -15,12 +15,35 @@ simple procedural script that is manually provided with inputs
 GCP_MT = "gs://cpg-acute-care-main/mt/acute-care.mt"
 GCP_ACUTE_TEST_BASE = "gs://cpg-acute-care-test"
 
-# --- Manually entered inputs --- #
-# note ^^ check which form of ID is present in the MT (looks like these IDs will be CPG internal IDs
-# also check whether these IDs can/should be committed to a public repo
+# --- Manually entered inputs, generated from families_to_samples.py --- #
+# **check which form of ID is present in the MT (looks like these IDs will be CPG internal IDs
+# **also check whether these IDs can/should be committed to a public repo
 family_sample_lookups = {
-    "1": {"p1", "m1", "f1"},
-    "2": {"p2", "m2", "f2"}
+    "FAM000340": [
+        "CPG54601",
+        "CPG54593",
+        "CPG54619"
+    ],
+    "FAM000320": [
+        "CPG54130",
+        "CPG54114",
+        "CPG54122"
+    ],
+    "FAM000337": [
+        "CPG54692",
+        "CPG54700",
+        "CPG54684"
+    ],
+    "FAM000327": [
+        "CPG54676",
+        "CPG54650",
+        "CPG54668"
+    ],
+    "FAM000334": [
+        "CPG54627",
+        "CPG54635",
+        "CPG54643"
+    ]
 }
 
 # collect all unique sample IDs for a single filter on the MT
@@ -33,11 +56,13 @@ mt = hl.read_matrix_table(GCP_MT)
 mt = mt.filter_cols(hl.literal(all_samples).contains(mt["s"]))
 
 # if we want to implement a region filter, e.g. MANE plus clinical, this would be the ideal time
+# current thinking is that it's not necessary at this time, as the case-specific work will determine
+# the regions of interest, etc.
 
 # for each family, dump both a small MT and a VCF containing the same samples/variants
 for family, samples in family_sample_lookups.items():
     samples_to_retain = hl.literal(samples)
-    # based on the implementation of hl.import_vcf, the deafult location for the sample ID would be the attribute 's'
+    # based on the implementation of hl.import_vcf, the default location for the sample ID would be the attribute 's'
     family_mt = mt.filter_cols(samples_to_retain.contains(mt["s"]))
 
     # write this family MT to a test location
