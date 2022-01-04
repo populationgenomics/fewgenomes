a98389d0550a3256d48646e71aafc1bdbb6a92a4
MattWellie <mattwellie@gmail.com>
2022-01-04 13:15:02 +1100

    shredded the original versions, now both take click arguments for all IDs

--- datasets/acute-care/extract_trio_vcf.py
+++ datasets/acute-care/extract_trio_vcf.py
@@ -1,72 +1,60 @@
+import click
 import hail as hl
+import json
 import os
 
 from itertools import chain
 
 
 """
-simple procedural script that is manually provided with inputs
-1. the family/families we're interested in retaining for the joint call (family ID and samples)
-2. the path to the MT from the full project joint call
-3. the path to the test output location, for storing the single-family VCF and MT
+simple procedural script that is provided with command line inputs
+- the JSON dictionary representing "family_name_1: [list, of, internal, sample, IDs], "
+- the project/dataset name, used to identify the buckets assc. with the specific project
+
+arguments passing mediated with click, so minimal hard-coding required 
 """
 
 
-GCP_MT = "gs://cpg-acute-care-main/mt/acute-care.mt"
-GCP_ACUTE_TEST_BASE = "gs://cpg-acute-care-test"
+@click.command()
+@click.option('--json-str', help='this is the formatted json string of family_sample_lookups', type=click.STRING)
+@click.option('--dataset', help="name of the dataset to use (gcp bucket names)", type=click.STRING)
+def main(json_str: str, dataset: str):
 
-# --- Manually entered inputs, generated from families_to_samples.py --- #
-# **check which form of ID is present in the MT (looks like these IDs will be CPG internal IDs
-# **also check whether these IDs can/should be committed to a public repo
-family_sample_lookups = {
-    "FAM000340": [
-        "CPG54601",
-        "CPG54593",
-        "CPG54619"
-    ],
-    "FAM000320": [
-        "CPG54130",
-        "CPG54114",
-        "CPG54122"
-    ],
-    "FAM000337": [
-        "CPG54692",
-        "CPG54700",
-        "CPG54684"
-    ],
-    "FAM000327": [
-        "CPG54676",
-        "CPG54650",
-        "CPG54668"
-    ],
-    "FAM000334": [
-        "CPG54627",
-        "CPG54635",
-        "CPG54643"
-    ]
-}
+    # parse the families dict from the input string, e.g. '{"fam1":["sam1","sam2"]}'
+    families_dict = json.loads(json_str)
 
-# collect all unique sample IDs for a single filter on the MT
-all_samples = set(chain.from_iterable(family_sample_lookups.values()))
+    gcp_test = f"gs://cpg-{dataset}-test"
+    gcp_main = f"gs://cpg-{dataset}-main"
+    gcp_mt_full = os.path.join(gcp_main, "mt", f"{dataset}.mt")
 
-# ok, so we need to pull in the massive MT
-mt = hl.read_matrix_table(GCP_MT)
+    # collect all unique sample IDs for a single filter on the MT
+    all_samples = set(chain.from_iterable(families_dict.values()))
 
-# filter the massive dataset to only the samples we're interested in
-mt = mt.filter_cols(hl.literal(all_samples).contains(mt["s"]))
+    # open full MT (note, not all read in, this is done lazily with spark)
+    mt = hl.read_matrix_table(gcp_mt_full)
 
-# if we want to implement a region filter, e.g. MANE plus clinical, this would be the ideal time
-# current thinking is that it's not necessary at this time, as the case-specific work will determine
-# the regions of interest, etc.
+    # filter the full dataset to only the samples we're interested in
+    mt = mt.filter_cols(hl.literal(all_samples).contains(mt["s"]))
 
-# for each family, dump both a small MT and a VCF containing the same samples/variants
-for family, samples in family_sample_lookups.items():
-    samples_to_retain = hl.literal(samples)
-    # based on the implementation of hl.import_vcf, the default location for the sample ID would be the attribute 's'
-    family_mt = mt.filter_cols(samples_to_retain.contains(mt["s"]))
+    # if we want to implement a region filter, e.g. MANE plus clinical, this would be the ideal time
+    # current thinking is that it's not necessary at this time, as the case-specific work will determine
+    # the regions of interest, etc.
 
-    # write this family MT to a test location
-    family_mt.write(os.path.join(GCP_ACUTE_TEST_BASE, f"{family}.mt"))
+    # for each family, dump both a small MT and a VCF containing the same samples/variants
+    for family, samples in families_dict.items():
 
-    # revert to a VCF file format, and write to a test location
-    hl.export_vcf(os.path.join(GCP_ACUTE_TEST_BASE, f"{family}.vcf.bgz"))
+        # take the list of samples and translate to a hail expression
+        samples_to_retain = hl.literal(samples)
+
+        # based on defaults for hl.import_vcf, location for the sample ID would be the attribute 's'
+        family_mt = mt.filter_cols(samples_to_retain.contains(mt["s"]))
+
+        # write this family MT to a test location
+        family_mt.write(os.path.join(gcp_test, f"{family}.mt"))
+
+        # revert to a VCF file format, and write to a test location
+        hl.export_vcf(os.path.join(gcp_test, f"{family}.vcf.bgz"))
+
+
+if __name__ == "__main__":
+    main()
--- datasets/acute-care/families_to_samples.py
+++ datasets/acute-care/families_to_samples.py
@@ -1,4 +1,6 @@
+import click
 import json
+import pandas
 import pandas as pd
 import requests
 import subprocess
@@ -9,9 +11,13 @@ from requests.structures import CaseInsensitiveDict
 
 
 """
-for some reason I can't get the sample-metadata API client installed and working locally
-this is a really rubbish workaround, and was probably a massive waste of time... 
-it was easier than playing around with the same endpoints in Swagger though
+this is a requests-centric process which takes a number of Family IDs, and returns a JSON string
+the output string contains a dictionary of each family ID to a set of identifiers
+by default these identifiers are CPG internal sample IDs
+by argument these can be external IDs instead 
+
+This is a lil script for finding samples belonging in each family. This would be faster and more robust as a DB query
+if this turns out to be useful to anyone, the same function should be recreated in the sample-metadata api repo
 """
 
 
@@ -46,12 +52,134 @@ def get_response(url, headers, query_params=None) -> requests.Response:
     return resp
 
 
-# this is an update-able variable,
-INTERESTING_FAMILIES = {"FAM000320", "FAM000327", "FAM000334", "FAM000337", "FAM000340"}
+def get_family_to_sample_map(df: pandas.DataFrame, families: str, external: bool, pid_to_id: dict, int_to_ext: dict
+                             ) -> dict:
+    """
+    take an arb. number of families from the command line, and for each, find all sample IDs. Return as a JSON dict
+    :param df: the pedigrees dataframe
+    :param families: str, a comma-delimited string containing family IDs
+    :param external: bool, True if we want to return external sample IDs, false for CPG IDs
+    :param pid_to_id: dict, lookup of int. participant ID to int. sample ID
+    :param int_to_ext: dict, lookup of int. sample ID to external
 
-# here we will build a set of the participant IDs within these families, using the mapping endpoints
-CHOSEN_INDIVIDUALS = set()
-CHOSEN_INDIVIDUALS_PER_FAMILY = {}
+    NOTE; this is all assuming a single participant-sample connection in the API results
+    """
+
+    dict_result = {}
+
+    for family_id in families.split(","):
+
+        dict_result[family_id] = []
+
+        # isolate all rows containing the family members from the pedigree table
+        members_df = df.loc[df["Family ID"] == family_id]
+        original_ids = {row["Individual ID"] for index, row in members_df.iterrows()}
+
+        # translate those IDs to something usable
+        for member_id in original_ids:
+
+            # participant to sample
+            int_sample_id = pid_to_id.get(member_id)
+
+            # internal sample to external sample
+            ext_sample_id = int_to_ext.get(int_sample_id)
+
+            # store the appropriate value, depending on argument flags
+            if external:
+                dict_result[family_id].append(ext_sample_id)
+            else:
+                dict_result[family_id].append(int_sample_id)
+
+    return dict_result
+
+
+@click.command()
+@click.option(
+    "--project",
+    "project",
+    type=click.STRING,
+    help="the name of the project to use in API queries"
+)
+@click.option(
+    "--families",
+    "families",
+    type=click.STRING,
+    help="a comma-delimited string, containing all the family IDs to search for"
+)
+@click.option(
+    "--external",
+    "external",
+    type=click.BOOL,
+    default=False,
+    is_flag=True,
+    help="if this is set, return external IDs instead"
+)
+def main(project: str, families: str, external: bool):
+    # only get this once, for now
+    auth_token = get_auth()
+
+    # currently the 2 types of endpoint require different headers, with the same auth token
+    table_header = CaseInsensitiveDict(data={"Accept": ACCEPT_ALL, "Authorization": f"Bearer {auth_token}"})
+    mapping_header = CaseInsensitiveDict(data={"Accept": ACCEPT_JSON, "Authorization": f"Bearer {auth_token}"})
+
+    # core query params from documentation
+    # get the pedigrees across the entire project
+    response = get_response(
+        url=f"{URL_BASE}/{FAMILY_ENDPOINT}/{project}/{PEDIGREE}",
+        headers=table_header,
+        query_params={
+            "replace_with_participant_external_ids": True,
+            "replace_with_family_external_ids": True,  # False
+            "empty_participant_value": "",
+            "include_header": True
+        }
+    )
+
+    # parse this into a series of dictionaries, then into a dataframe
+    dict_read = DictReader(StringIO(response.content.decode().strip("#")), delimiter="\t")
+    pedigree_df = pd.DataFrame([line for line in dict_read])
+
+    # -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#- #
+    # so now we have a dataframe of the entire pedigree structure
+    # NOTE - this is just a hack around, with examples of further hackery
+
+    # # example queries on pedigree_df - all affected females
+    # aff_females = pedigree_df.loc[(pedigree_df["Affected"] == "2") & (pedigree_df["Sex"] == "2")]
+    #
+    # # all members of those families
+    # all_aff_female_families = pedigree_df.loc[pedigree_df["Family ID"].isin(aff_females["Family ID"].values)]
+    #
+    # # are there any affected parents? - affected and not "proband" in name (should be none in acute care)
+    # aff_parents = pedigree_df.loc[
+    #   (pedigree_df["Affected"] == "2") &
+    #   (~pedigree_df["Individual ID"].str.contains("proband")),
+    #   ["Family ID", "Individual ID"]
+    # ]
+    # -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#- #
+
+    # we can obtain a lookup on the internal sample ID using the external PID
+    # participant/acute-care/external-pid-to-internal-sample-id # JSON
+    # this will get us a CPG ID (INTernal, not INTeger)
+    response = get_response(url=f"{URL_BASE}/{PARTICIPANT_ENDPOINT}/{project}/external-pid-to-internal-sample-id",
+                            headers=mapping_header)
+    paired_samples_list = response.json()
+    pid_to_sample_id = dict(zip([_[0] for _ in paired_samples_list], [_[1] for _ in paired_samples_list]))
+
+    # we can then map the internal sample IDs to external sample IDs
+    # sample/acute-care/id-map/internal/all # JSON
+    response = get_response(url=f"{URL_BASE}/{SAMPLE_ENDPOINT}/{project}/id-map/internal/all", headers=mapping_header)
+    internal_to_ext_map = response.json()
+
+    family_to_samples_dict = get_family_to_sample_map(
+        df=pedigree_df,
+        families=families,
+        external=external,
+        pid_to_id=pid_to_sample_id,
+        int_to_ext=internal_to_ext_map
+    )
+
+    # now print as a string value, with no spaces
+    print(json.dumps(family_to_samples_dict, separators=(',', ':')))
 
 
 URL_BASE = "https://sample-metadata-api-mnrpw3mdza-ts.a.run.app/api/v1"
@@ -62,104 +190,6 @@ PARTICIPANT_ENDPOINT = "participant"
 SAMPLE_ENDPOINT = "sample"
 PEDIGREE = "pedigree"
 
-# subject to flexibility here
-PROJECT = "acute-care"
 
-# only get this once, for now
-AUTH_TOKEN = get_auth()
-
-table_header = CaseInsensitiveDict(
-    data={
-        "Accept": ACCEPT_ALL,
-        "Authorization": f"Bearer {AUTH_TOKEN}"
-    }
-)
-
-mapping_header = CaseInsensitiveDict(
-    data={
-        "Accept": ACCEPT_JSON,
-        "Authorization": f"Bearer {AUTH_TOKEN}"
-    }
-)
-
-# core query params from documentation
-# get the pedigrees across the entire project
-response = get_response(
-    url=f"{URL_BASE}/{FAMILY_ENDPOINT}/{PROJECT}/{PEDIGREE}",
-    headers=table_header,
-    query_params={
-        "replace_with_participant_external_ids": True,
-        "replace_with_family_external_ids": True,
-        "empty_participant_value": "",
-        "include_header": True
-    }
-)
-
-# parse this into a series of dictionaries, then into a dataframe
-dict_read = DictReader(StringIO(response.content.decode().strip("#")), delimiter="\t")
-df = pd.DataFrame([line for line in dict_read])
-
-# -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#- #
-# so now we have a dataframe of the entire pedigree structure
-# NOTE - this is just a hack around, with examples of further hackery
-
-# # example queries on df - all affected females
-# aff_females = df.loc[(df["Affected"] == "2") & (df["Sex"] == "2")]
-#
-# # all members of those families
-# all_aff_female_families = df.loc[df["Family ID"].isin(aff_females["Family ID"].values)]
-#
-# # are there any affected parents? - affected and not "proband" in name (should be none in acute care)
-# aff_parents = df.loc[
-#   (df["Affected"] == "2") &
-#   (~df["Individual ID"].str.contains("proband")),
-#   ["Family ID", "Individual ID"]
-# ]
-# -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#- #
-
-# ok, here's an exploratory route
-# choose a random ID, obtained by translating the incremented ID to external ID
-selected_id = df.iloc[1]["Individual ID"]
-
-# we can obtain a lookup on the internal sample ID using the external PID
-# participant/acute-care/external-pid-to-internal-sample-id # JSON
-# this will get us a CPG ID (INTernal, not INTeger)
-response = get_response(url=f"{URL_BASE}/{PARTICIPANT_ENDPOINT}/{PROJECT}/external-pid-to-internal-sample-id", headers=mapping_header)
-paired_samples_list = response.json()
-
-## USEFUL MAPPING
-pid_to_sample_id = dict(zip([_[0] for _ in paired_samples_list], [_[1] for _ in paired_samples_list]))
-
-# we can then map the internal sample IDs to external sample IDs
-# sample/acute-care/id-map/internal/all # JSON
-response = get_response(url=f"{URL_BASE}/{SAMPLE_ENDPOINT}/{PROJECT}/id-map/internal/all", headers=mapping_header)
-
-## USEFUL MAPPING
-internal_to_ext_map = response.json()
-
-# iterate over each family ID, getting the external sample IDs for each participant
-for each_family in INTERESTING_FAMILIES:
-
-    CHOSEN_INDIVIDUALS_PER_FAMILY[each_family] = []
-
-    # isolate all rows containing the family members from the pedigree table
-    members_df = df.loc[df["Family ID"] == each_family]
-    original_ids = {row["Individual ID"] for index, row in members_df.iterrows()}
-
-    # now translate those IDs to something usable
-    for member_id in original_ids:
-        # participant to sample
-        int_sample_id = pid_to_sample_id.get(member_id)
-        # internal sample to external sample
-        ext_sample_id = internal_to_ext_map.get(int_sample_id)
-        # store
-        CHOSEN_INDIVIDUALS.add(int_sample_id)
-        CHOSEN_INDIVIDUALS_PER_FAMILY[each_family].append(int_sample_id)
-
-# this is the form we're taking for the trio extraction input
-print(json.dumps(CHOSEN_INDIVIDUALS_PER_FAMILY, indent=4))
-
-# # we could then get all details for those samples
-# # sample/acute-care/<ext sample id>/details # JSON
-# response = get_response(url=f"{URL_BASE}/{SAMPLE_ENDPOINT}/{PROJECT}/{ext_sample_id}/details", headers=mapping_header)
-# print(response.json())
+if __name__ == "__main__":
+    main()
