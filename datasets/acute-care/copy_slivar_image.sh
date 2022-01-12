0c6c56fb240579d38ee8c8afbb25b4211bb408ae
MattWellie <mattwellie@gmail.com>
2022-01-13 10:28:17 +1100

    singlequotes, pinned container

--- datasets/acute-care/copy_slivar_image.sh
+++ datasets/acute-care/copy_slivar_image.sh
@@ -3,4 +3,4 @@
 set -ex
 
 gcloud auth configure-docker australia-southeast1-docker.pkg.dev
-skopeo copy docker://docker.io/brentp/slivar:latest docker://australia-southeast1-docker.pkg.dev/cpg-common/images/slivar
+skopeo copy docker://docker.io/brentp/slivar:v0.2.7 docker://australia-southeast1-docker.pkg.dev/cpg-common/images/slivar:v0.2.7
