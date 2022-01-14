57437f88783021b8edd3ef42249a8f49af120d54
MattWellie <mattwellie@gmail.com>
2022-01-14 16:18:28 +1100

    vep job inside batch didn't work

--- datasets/acute-care/run_vep_on_input.py
+++ datasets/acute-care/run_vep_on_input.py
@@ -6,8 +6,7 @@ execute a subprocess call in python to run VEP annotation
 """
 
 
-import os
-import hailtop.batch as hb
+import subprocess
 import click
 
 
@@ -29,33 +28,12 @@ def main(infile: str, outfile: str):
     :param outfile: str, the path to the VEP annotated output VCF
     """
 
-    # start a hail batch
-    service_backend = hb.ServiceBackend(
-        billing_project=os.getenv('HAIL_BILLING_PROJECT'),
-        bucket=os.getenv('HAIL_BUCKET'),
-    )
+    complete_cmd_string = f'/vep --format vcf -i {infile} ' \
+                          f'--everything --allele_number --no_stats ' \
+                          f'--cache --offline --minimal --assembly ' \
+                          f'GRCh38 --vcf -o {outfile}'
 
-    # create a hail batch
-    batch = hb.Batch(
-        name='run_vep',
-        backend=service_backend
-    )
-
-    job = batch.new_job(name='run vep')
-    job.cpu(2)
-    job.memory('standard')  # ~ 4G/core ~ 7.5G
-    job.storage('20G')
-
-    # read in from GC file
-    in_vcf = batch.read_input(infile)
-
-    job.command(
-        f'/vep --format vcf -i {in_vcf} --everything '
-        f'--allele_number --no_stats --cache --offline '
-        f'--minimal --assembly GRCh38 --vcf -o {job.ofile}'
-    )
-    batch.write_output(job.ofile, outfile)
-    batch.run(wait=False)
+    subprocess.check_output(complete_cmd_string.split())
 
 
 if __name__ == '__main__':
