04d1da178c820c7512804f7b6c2a76e9d991c427
MattWellie <mattwellie@gmail.com>
2022-01-16 23:19:11 +1100

    filter non-genics

--- datasets/acute-care/analyse_mt.py
+++ datasets/acute-care/analyse_mt.py
@@ -3,12 +3,59 @@
 
 """
 Loads in the MatrixTable, and prints out a description of the cols
+
+Sample Genotype is annotated at the Entry level 'GT'
+Only interested in genes with vep.transcript_consequences.gene_id
+perhaps a filter for 'not present' or ''
+A threshold
+
+Filter syntax example
+mt = mt.filter_rows(dataset.variant_qc.AF[1] < 0.01, keep=True)
 """
 
 
 import logging
+from typing import Optional
 import hail as hl
 import click
+
+
+def go_and_get_mt(mt_path: str) -> hl.MatrixTable:
+    """
+    Reads in the stored MatrixTable from disk
+    :param mt_path: str, path to a MT directory
+    """
+
+    annotated_mt = hl.read_matrix_table(mt_path)
+    return annotated_mt
+
+
+def remove_non_genic_variants(
+        matrix: hl.MatrixTable,
+        keep_genes: Optional[set] = None
+) -> hl.MatrixTable:
+    """
+    either remove all variants without gene annotations
+    :param matrix: hl.MatrixTable
+    :param keep_genes: Optional[set]
+    """
+
+    logging.info('Number of variants prior to filtering: %d', matrix.count_rows())
+
+    if keep_genes is None:
+        keep_genes = set()
+
+    if len(keep_genes) == 0:
+        filtered_matrix = matrix.filter_rows(
+            matrix.transcript_consequences.gene_id in keep_genes
+        )
+    else:
+        filtered_matrix = matrix.filter_rows(
+            matrix.transcript_consequences.gene_id in {None, ''}
+        )
+
+    logging.info('Number of variants prior to filtering: %d', filtered_matrix.count_rows())
+    return filtered_matrix
 
 
 @click.command()
@@ -34,11 +81,15 @@ def main(matrix: str, conf: str, reference: str):
     :param conf: str, path to a json file configuring the analysis
     :param reference: str, path to a script to run inside the dataproc
     """
+
+    # boot up hail with the relevant reference
     hl.init(default_reference=reference)
 
     logging.info('Config path: %s', conf)
-    annotated_mt = hl.read_matrix_table(matrix)
-    print(annotated_mt.col.describe())
+    annotated_mt = go_and_get_mt(mt_path=matrix)
+
+    remove_non_genic_variants(matrix=annotated_mt)
+
     print(annotated_mt.describe())
 
 
