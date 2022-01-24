40c8869ad9ce12aef1d7ea1f65cbd36b9886edeb
Sabrina Yan <sabrina.yan@populationgenomics.org.au>
2022-01-24 12:39:29 +1100

    Done, first hail batch scripts

--- batch/hello_world.py
+++ batch/hello_world.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import argparse
-from tkinter import N
 import hailtop.batch as hb
 
 
@@ -14,15 +13,20 @@ def main(args):
         scatter_gather,
         file_gather,
         nested_scatter,
+        nested_scatter_functions,
+        input_file,
+        output_file,
+        python_job,
     ]
 
-    if args.list:
+    if args.list or not args.example:
         help(examples)
         return
 
-    n = args.example - 1
     try:
-        examples[n]()
+        if args.example:
+            n = args.example - 1
+            examples[n]()
     except:
         help(examples)
 
@@ -30,7 +34,7 @@ def main(args):
 def help(examples):
     print(f'What example do you want to run?')
     for i, x in enumerate(examples):
-        print(f'{i+1}) {x.__name__}')
+        print(f'{i+1: >3}) {x.__name__}')
 
     print(f'Provide a number [1-{len(examples)}] as an argument to run')
 
@@ -107,6 +111,63 @@ def nested_scatter():
         for chore in ['make-bed', 'laundry', 'grocery-shop']:
             j = b.new_job(name=f'{user}-{chore}')
             j.command(f'echo "user {user} is doing chore {chore}"')
+    b.run()
+
+
+def do_chores(b, head, user):
+    chores = []
+    for chore in ['make-bed', 'laundry', 'grocery-shop']:
+        j = b.new_job(name=f'{user}-{chore}')
+        j.command(f'echo "user {user} is doing chore {chore}"')
+        j.depends_on(head)
+        chores.append(j)
+    sink = b.new_job(name=f'{user}-sink')
+    sink.depends_on(*chores)
+    return sink
+
+
+def nested_scatter_functions():
+    b = hb.Batch(name='nested-scatter-3')
+    head = b.new_job(name='head')
+    user_sinks = []
+    for user in ['Alice', 'Bob', 'Dan']:
+        user_sink = do_chores(b, head, user)
+        user_sinks.append(user_sink)
+    final_sink = b.new_job(name='final-sink')
+    final_sink.depends_on(*user_sinks)
+    b.run()
+
+
+def input_file():
+    b = hb.Batch(name='hello-input')
+    input = b.read_input('hello.txt')
+    j = b.new_job(name='hello')
+    j.command(f'cat {input}')
+    b.run()
+
+
+def output_file():
+    b = hb.Batch(name='hello-input')
+    j = b.new_job(name='hello')
+    j.command(f'echo "hello" > {j.ofile}')
+    b.write_output(j.ofile, 'output/hello.txt')
+    b.run()
+
+
+def hello_world(name):
+    return f'hello {name}'
+
+
+def upper(s):
+    return s.upper()
+
+
+def python_job():
+    b = hb.Batch(name='python-job')
+    j = b.new_python_job(name='hello')
+    hello_str = j.call(hello_world, 'alice')
+    result = j.call(upper, hello_str)
+    b.write_output(result.as_str(), 'output/alice.txt')
     b.run()
 
 
