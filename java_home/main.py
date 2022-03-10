a2fcd4af4185d97db855694b24cb4115e536ed02
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-03-10 17:39:11 +1100

    await again

--- java_home/main.py
+++ java_home/main.py
@@ -3,6 +3,9 @@
 import hail as hl
 import os
 
-hl.init_service(default_reference='GRCh38', billing_project=os.getenv('HAIL_BILLING_PROJECT'))
-hl.import_table('test.csv')
+def main():
+    await hl.init_service(default_reference='GRCh38', billing_project=os.getenv('HAIL_BILLING_PROJECT'))
+    hl.import_table('test.csv')
 
+if __name__ == '__main__':
+    main()
