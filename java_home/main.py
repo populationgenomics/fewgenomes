3bac65363b1e80b8c2c5d8f7dfebcb114ecd8c3c
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-03-10 18:03:00 +1100

    More hailctl

--- java_home/main.py
+++ java_home/main.py
@@ -5,6 +5,8 @@ import os
 import subprocess
 
 subprocess.run(['hailctl', 'config', 'set', 'batch/billing_project', os.getenv('HAIL_BILLING_PROJECT')], check=True)
+subprocess.run(['hailctl', 'config', 'set', 'batch/remote_tmpdir', os.getenv('HAIL_BUCKET')], check=True)
+
 hl.init(default_reference='GRCh38')
 hl.import_table('test.csv')
 
