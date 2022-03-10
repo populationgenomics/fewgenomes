188a6dcb5015b79f3bbd5a6917bae526bd43a72b
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-03-10 17:13:33 +1100

    Print HAIL_QUERY_BACKEND

--- java_home/main.py
+++ java_home/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import hail as hl
+import os
+
+print(f'HAIL_QUERY_BACKEND={os.getenv("HAIL_QUERY_BACKEND")}')
 
 hl.init(default_reference='GRCh38')
 hl.import_table('test.csv')
