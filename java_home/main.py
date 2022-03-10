1496d7b6ff74c0ba173b4f14471ed5e7fb558716
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-03-10 18:05:42 +1100

    Bucket path

--- java_home/main.py
+++ java_home/main.py
@@ -5,7 +5,8 @@ import os
 import subprocess
 
 subprocess.run(['hailctl', 'config', 'set', 'batch/billing_project', os.getenv('HAIL_BILLING_PROJECT')], check=True)
-subprocess.run(['hailctl', 'config', 'set', 'batch/remote_tmpdir', os.getenv('HAIL_BUCKET')], check=True)
+hail_bucket = os.getenv('HAIL_BUCKET')
+subprocess.run(['hailctl', 'config', 'set', 'batch/remote_tmpdir', f'gs://{hail_bucket}/batch-tmp'], check=True)
 
 hl.init(default_reference='GRCh38')
 hl.import_table('test.csv')
