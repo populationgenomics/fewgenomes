f87188853db37cc52ea052e41dd9e0c24eb18f93
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-03-10 17:54:27 +1100

    asyncio

--- java_home/main.py
+++ java_home/main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
+import asyncio
 import hail as hl
 import os
 
-def main():
+async def main():
     await hl.init_service(default_reference='GRCh38', billing_project=os.getenv('HAIL_BILLING_PROJECT'))
     hl.import_table('test.csv')
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
