67766e78f07fee3711c707f7d53776d09c996e4f
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-03-11 16:25:49 +1100

    Add to/from_pandas

--- java_home/main.py
+++ java_home/main.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
+import asyncio
 import hail as hl
 import os
-import subprocess
 
-subprocess.run(['hailctl', 'config', 'set', 'batch/billing_project', os.getenv('HAIL_BILLING_PROJECT')], check=True)
-hail_bucket = os.getenv('HAIL_BUCKET')
-subprocess.run(['hailctl', 'config', 'set', 'batch/remote_tmpdir', f'gs://{hail_bucket}/batch-tmp'], check=True)
+def init_hail_query_service():
+    billing_project = os.getenv('HAIL_BILLING_PROJECT')
+    hail_bucket = os.getenv('HAIL_BUCKET')
+    asyncio.get_event_loop().run_until_complete(hl.init_service(default_reference='GRCh38', billing_project=billing_project, remote_tmpdir=f'gs://{hail_bucket}/batch-tmp'))
 
-hl.init(default_reference='GRCh38')
+init_hail_query_service()
 t = hl.import_table('gs://cpg-fewgenomes-test/benchmark/outputs/NA12340/duplicate-metrics/NA12340-duplicate-metrics.csv')
 t.describe()
+t.show()
+pd = t.to_pandas()
+t2 = pd.from_pandas()
+t2.describe()
+t2.show()
 
