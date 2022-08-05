a7ef971a461b665408ce32ae2203d1c49f65614e
Michael Franklin <michael.franklin@populationgenomics.org.au>
2022-08-05 17:13:08 +1000

    Actually run script

--- test/test_requester_pays.py
+++ test/test_requester_pays.py
@@ -5,8 +5,13 @@ from shlex import quote
 from cpg_utils.hail_batch import copy_common_env, get_config, remote_tmpdir
 import hailtop.batch as hb
 
+config = get_config()
+
 HAIL_PROJECT = 'hail-295901'
-DRIVER_IMAGE = 'australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:646dcc2ab7e484314943b979d2132b0133dbbe94-hail-b28ed8c6a7f8dadc9275a87064b5ccfe562238c3'
+DRIVER_IMAGE = config.get('workflow', {}).get(
+    'driver_image',
+    'australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:646dcc2ab7e484314943b979d2132b0133dbbe94-hail-b28ed8c6a7f8dadc9275a87064b5ccfe562238c3',
+)
 GCLOUD_INIT = 'gcloud -q auth activate-service-account --key-file=/gsa-key/key.json'
 
 requester_pays_base = 'gs://cpg-mfranklin-requester-pays'
@@ -31,10 +36,12 @@ jobs = [
 #     billing_project='michaelfranklin-trial', remote_tmpdir='gs://cpg-michael-hail-dev/tmp/'
 # )
 sb = hb.ServiceBackend(
-    billing_project=get_config()["hail"]["billing_project"],
+    billing_project=config["hail"]["billing_project"],
     remote_tmpdir=remote_tmpdir(),
 )
-batch = hb.Batch('requester-pays-tests', backend=sb, requester_pays_project=HAIL_PROJECT)
+batch = hb.Batch(
+    'requester-pays-tests', backend=sb, requester_pays_project=HAIL_PROJECT
+)
 
 if JOBS_COPY_IN in jobs:
     j_copy_in = batch.new_job(JOBS_COPY_IN)
@@ -45,7 +52,9 @@ if JOBS_COPY_OUT in jobs:
     j_copy_out = batch.new_job('copy-out')
     copy_out_file = j_copy_out.outfile
     j_copy_out.command(f'echo "Copy out" > {copy_out_file}')
-    batch.write_output(copy_out_file, os.path.join(requester_pays_base, 'copy-output.txt'))
+    batch.write_output(
+        copy_out_file, os.path.join(requester_pays_base, 'copy-output.txt')
+    )
 
 if JOBS_STREAM_IN in jobs:
     j_stream_in = batch.new_job(JOBS_STREAM_IN)
@@ -66,8 +75,9 @@ if JOBS_HAIL_QUERY in jobs:
     j_hq.image(DRIVER_IMAGE)
     j_hq.command(GCLOUD_INIT)
     copy_common_env(j_hq)
-    j_hq.command(f"""
-cat <<EOF
+    j_hq.command(
+        f"""
+cat > script.py <<EOF 
 import hail as hl
 
 hl.init_batch(
@@ -80,7 +90,10 @@ mt = hl.read_matrix_table('gs://cpg-fewgenomes-test/mt')
 
 mt.show()
 EOF
-""")
+
+python script.py
+"""
+    )
 
 
 if len(batch._jobs) == 0:
