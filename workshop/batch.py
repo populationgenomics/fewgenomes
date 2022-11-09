5d95f0fa764112b9e950fdcff64c053ca220d5e1
Michael Franklin <michael.franklin@populationgenomics.org.au>
2022-11-09 23:02:53 +1100

    Fix my mistakes

--- workshop/batch.py
+++ workshop/batch.py
@@ -5,9 +5,8 @@ from cpg_utils.hail_batch import get_config, remote_tmpdir
 config = get_config()
 
 sb = hb.ServiceBackend(
-   billing_project=config['hail']['billing_project'],
-   remote_tmpdir=remote_tmpdir(),
-   
+    billing_project=config['hail']['billing_project'],
+    remote_tmpdir=remote_tmpdir(),
 )
 
 name = 'YOURNAME'
@@ -16,3 +15,5 @@ b = hb.Batch(name=f'{name}: Hello Batch', backend=sb)
 job = b.new_bash_job('Print my name')
 
 job.command(f'echo "Hello, {name}!"')
+
+b.run(wait=False)
