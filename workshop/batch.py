6e36ef5d6174af41441c19f1b56ce0fa05140815
Michael Franklin <michael@illusional.net>
2022-11-16 08:22:25 +1100

    Strip back to template

--- workshop/batch.py
+++ workshop/batch.py
@@ -12,11 +12,6 @@ sb = hb.ServiceBackend(
 name = 'YOURNAME'
 b = hb.Batch(name=f'{name}: Hello Batch', backend=sb)
 
-job = b.new_bash_job('Print my name')
-job_output_1 = job.my_tdout
-job.command(f'echo "Hello, {name}!" > {job_output_1}')
-
-job2 = b.new_bash_job('Print contents of job1')
-job2.command(f'cat {job_output_1}')
+# your code here
 
 b.run(wait=False)
