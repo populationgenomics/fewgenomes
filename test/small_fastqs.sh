31411cd47844318a9db4e401303ae35e1e5af301
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2023-02-17 15:33:05 +1100

    Create small FASTQs for a few public samples (#21)
    * Add small_fastqs script
    
    * Add script to create small FASTQs
    
    * Add forgotten script
    
    * Fix array
    
    * Fix samtools view
    
    * Set GCS_OAUTH_TOKEN
    
    * No tmp
    
    * Use local storage
    
    * Install google-crc32c
    
    * Fix crc32 install
    
    * Try gsutil
    
    * Fix token refresh

---
+++ test/small_fastqs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Creates mini FASTQs from a few public 1KG samples.
+# 
+# Run as follows:
+# 
+# analysis-runner \
+#     --dataset fewgenomes \
+#     --access-level test \
+#     --description "create small FASTQs for testing" \
+#     --output small_fastqs \
+#     --image australia-southeast1-docker.pkg.dev/cpg-common/images/samtools:1.16.1 \
+#     small_fastqs.sh
+
+set -ex
+
+SAMPLES=(NA12340 NA12489 NA12878 NA12891 NA12892)
+CRAM_PREFIX="gs://cpg-fewgenomes-test/cram"
+OUTPUT_PREFIX="gs://cpg-fewgenomes-test/small_fastqs"
+# FLRT2 gene, but there's no significance to that.
+BED_INTERVAL="chr14 85530143 85654428"
+REFERENCE="gs://cpg-common-main/references/hg38/v0/Homo_sapiens_assembly38.fasta"
+
+# Localize the reference once.
+gsutil -m cp "$REFERENCE" "$REFERENCE.fai" .
+
+for SAMPLE in "${SAMPLES[@]}"; do
+    # Required by samtools to be able to read from GCS. Refresh this regularly.
+    GCS_OAUTH_TOKEN=$(gcloud auth application-default print-access-token)
+    export GCS_OAUTH_TOKEN
+    # Extract a genomic interval from the CRAM, then convert to FASTQs.
+    samtools view -L <(echo "$BED_INTERVAL") -b -T "$(basename $REFERENCE)" "$CRAM_PREFIX/$SAMPLE.cram" | \
+    samtools bam2fq -1 "$SAMPLE.R1.fastq.gz" -2 "$SAMPLE.R2.fastq.gz"
+    # Writing to GCS directly seems problematic, so copy over temporary local files.
+    gsutil -m mv "$SAMPLE.R?.fastq.gz" "$OUTPUT_PREFIX/"
+done
