73df31b3a9eae7619869e42a6a4806bea501c18c
Michael Franklin <illusional@users.noreply.github.com>
2023-09-12 10:29:57 +1000

    Add init

--- test/read_file.py
+++ test/read_file.py
@@ -1,5 +1,7 @@
 import hail as hl
 
+hl.init(default_reference='grch38')
+
 COVERAGE_FILE = "gs://gcp-public-data--gnomad/release/3.0.1/coverage/genomes/gnomad.genomes.r3.0.1.coverage.ht"
 ht = hl.read_table(COVERAGE_FILE)
 
