318a72c45a190ac6262eeb94a76cc88e25d3b936
Michael Franklin <illusional@users.noreply.github.com>
2023-09-12 10:31:23 +1000

    Fix casing

--- test/read_file.py
+++ test/read_file.py
@@ -1,6 +1,6 @@
 import hail as hl
 
-hl.init(default_reference='grch38')
+hl.init(default_reference='GRCh38')
 
 COVERAGE_FILE = "gs://gcp-public-data--gnomad/release/3.0.1/coverage/genomes/gnomad.genomes.r3.0.1.coverage.ht"
 ht = hl.read_table(COVERAGE_FILE)
